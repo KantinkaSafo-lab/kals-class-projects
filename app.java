4c8efbd44e3e3027d7aceb6284cb53c24de8547c
unknown <safowaaakua9@gmail.com>
2025-08-12 23:26:23 +0000

    java file added

---
+++ app.java
@@ -0,0 +1,16 @@
+//Im Akua, a contributor to this project.
+
+import java.util.Scanner;
+
+public class GreetUser {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello, " + name + "! Welcome to Java.");
+
+        scanner.close();
+    }
+}

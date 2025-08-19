def96823b60ffa0dcd98d4f5ac81f2a73f9f66f7
boahene <boahenkwasi569@gmail.com>
2025-08-19 07:13:00 +0000

    Boahene added a python file

---
+++ App (2).py
@@ -0,0 +1,19 @@
+name = "Akwasi Boahene is my name"
+print(name.upper())
+print(name.lower())
+print(name.title())
+print(name.capitalize())
+print(name.find("is"))
+print(name.count("Akwasi"))
+print(name.replace("my","your"))
+print(10/4)
+x = 10
+x = (x + 3) ** 2
+print(x)
+price = 5
+print(not price > 4)
+temperature = 23
+if temperature > 37:
+    print("It's a hot day so drink plenty water")
+elif temperature < 30:
+    print("It's a cold day so exercise to warm up your body")
